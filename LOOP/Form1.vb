ad04ced7b8f5f81762eae2456fe8f0b31f2165f2
Dan2TDMJace <112831108+Dan2TDMJace@users.noreply.github.com>
2022-09-22 16:44:20 -0500

    google.
    just google.

---
+++ LOOP/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub Button2_Click(sender As Object, e As EventArgs)
+        Do
+            MsgBox("This message was programmed on a loop while programming a program with a button that says Brainpop Loop that runs this message... on a loop.", 0 + 64, "LOOP")
+        Loop
+
+    End Sub
+
+End Class
---
+++ LOOP/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("LOOP")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("LOOP")>
+<Assembly: AssemblyCopyright("Copyright ©  2022")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("f66a2812-5df6-4801-919e-24c9db98db09")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
